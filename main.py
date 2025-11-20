1b98b0588a0965a098ef48eb26df22ce222acae2
Cassandra184 <239755962+Cassandra184@users.noreply.github.com>
2025-11-20 09:47:28 +0000

    Exercice nombre premier

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
-from math import sqrt
-
 #### Fonction secondaire
+"""
+Exercice permettant de savoir si un nombre est premier ou non 
 
-
+Modules:
+isprime(p)
+main()
+"""
 def isprime(p):
+    """
+    Args:
+    p: une chaine de caractères
 
-    # votre code ici
-
-    pass
+    Returns:
+    un booléen, true si le nombre est premier, false sinon
+    """
+    if p <= 1:
+        return False
+    if p == 2:
+        return True
+    if p % 2 == 0:
+        return False
+    for i in range(3, int(p**0.5) + 1, 2):
+        if p % i == 0:
+            return False
+    return True
 
 #### Fonction principale
 
-
 def main():
-
-    # vos appels à la fonction secondaire ici
+    """
+    Permet d'effectuer des tests pour des entiers de 0 à 100
+    """
 
     for n in range(100):
         if isprime(n):
